ad873250665977ebee8c277bc4ef6f9e8437fabe
Samantha Gatt <smfgatt@gmail.com>
2018-09-18 17:27:58 -0400

    Adds project start

---
+++ Firebase Chat/Firebase Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase Chat
+//
+//  Created by Samantha Gatt on 9/18/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
